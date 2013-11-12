f45e86dde87e8ce293c4c4b2d1bae500dde3bf6b
phurtado1112 <phurtado1112@gmail.com>
2013-11-11 18:56:33 -0600

    PHD

--- src/GUI/RepDocenteIF.java
+++ src/GUI/RepDocenteIF.java
@@ -60,10 +60,6 @@ public class RepDocenteIF extends javax.swing.JInternalFrame {
         }
     }
     
-    public void prueba(){
-        
-    }
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
